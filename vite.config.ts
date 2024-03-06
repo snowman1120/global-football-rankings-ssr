15bcbd63de6600ba22d294099ce405d923086a01
snowman1120 <snowman19901120@gmail.com>
2024-03-05 21:30:41 -0500

    fix app booting error and set proxy PORT in UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
 import path, { resolve } from 'path';
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-      '~/': `${path.resolve(__dirname, 'src')}/`,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+        '~/': `${path.resolve(__dirname, 'src')}/`,
+      },
     },
-  },
-})
+    server: {
+      proxy: {
+        '/api': {
+          target: `http://localhost:${process.env.VITE_PORT}`,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+      },
+    },
+  });
+}
